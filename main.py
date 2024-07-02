cf1c4cc124a6f5fe936157d7c39256c3ed664674
Yannick <132798545+Kunnak@users.noreply.github.com>
2024-07-02 16:50:25 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+
+Eine App die alle Skripte für die Expertenaufgaben beinhaltet!
+Die beinhalteten Skripte sind:
+
+    - Passwortprüfer
+    - Passwortgenerator
+    - Schiffe versenken
+    - Klick - Spiel
+    - Sportaktivitäten
+    - Glückliche Zahlen
+
+"""
+import GUI
+GUI.MyGridApp().run()
